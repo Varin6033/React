34ba9bdc2d96cb235440f7d5aafb83462efd25ae
Varin Upadhyay <varin6033@gmail.com>
2021-09-15 21:16:50 -0400

    Firsrt commit

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,15 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// JSX 
-ReactDOM.render(<ul>
-  <li>Varin</li>
-  <li>Maulik</li>
-  <li>Satishkumar</li>
-  </ul>
-,document.getElementById('root'));
+function MyInfo(){
+  return(
+    <div>
+      <h1>Varin Upadhyay</h1>
+      <p>I live in Toronto</p>
+      <ul>
+        <li>US</li>
+        <li>India</li>
+        <li>Japan</li>
+      </ul>
+    </div>
+  )
+}
 
-
+ReactDOM.render(<MyInfo />, document.getElementById("root"))
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
