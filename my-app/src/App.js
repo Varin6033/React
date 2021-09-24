b9f6fc06469d45da6c453f9c06400d61615cd65f
Varin Upadhyay <varin6033@gmail.com>
2021-09-24 00:03:31 -0400

    React props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,31 +4,29 @@ import './App.css';
 import Footer from "./Footer";
 import Header from "./Header";
 import MainContent from "./MainContent"
+import TodoItem from './components/TodoItem';
+import ContactCard from './components/ContactCard';
 
 function App() {
-  const date = new Date(2018, 6, 31, 15)
-  const hours = date.getHours()
-  let timeOfDay
-  const styles = {
-    fontSize: 40
-  }
-
-  if(hours < 12) {
-    timeOfDay = "morning"
-    styles.color = "black"
-  } else if(hours >= 12 && hours < 17){
-    timeOfDay = "afternoon"
-    styles.color = "blue"
-  } else {
-    timeOfDay = "night"
-    styles.color = "red"
-  }
-
-  
-
-  return (
-    <h1 style={styles}>Good {timeOfDay}</h1>
-  );
+  return(
+    <div className="contacts">
+            <ContactCard 
+                contact={{name: "Mr. Whiskerson", imgUrl: "http://placekitten.com/300/200", phone: "(212) 555-1234", email: "mr.whiskaz@catnap.meow"}}
+            />
+            
+            <ContactCard 
+                contact={{name: "Fluffykins", imgUrl: "http://placekitten.com/400/200", phone: "(212) 555-2345", email: "fluff@me.com"}}
+            />
+            
+            <ContactCard
+                contact={{name: "Destroyer", imgUrl: "http://placekitten.com/400/300", phone: "(212) 555-3456", email: "ofworlds@yahoo.com"}}
+            />
+            
+            <ContactCard 
+                contact={{name: "Felix", imgUrl: "http://placekitten.com/200/100", phone: "(212) 555-4567", email: "thecat@hotmail.com"}}
+            />
+    </div>
+  )
 }
 
 export default App;
