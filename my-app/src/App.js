296cddcb6ce6ff0da38f5d9ab94b3d8fdcb494bb
Varin Upadhyay <varin6033@gmail.com>
2021-10-28 22:10:15 -0400

    class based componenets

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Footer from "./Footer";
-import Header from "./Header";
-import MainContent from "./MainContent"
-import TodoItem from './components/TodoItem';
-import Joke from './components/Joke';
-import jokesData from './jokesData';
-import productsData from './vschoolProducts'
-import Product from './components/Product';
-import todosData from './todosData';
+import React, { Component } from 'react';
 
-function App() {
+// #1
+class App extends React.Component {
+  render(){
+    return (
+      <div>
+          <Header username="Varin"/>
+          <Greeting />
+      </div>
+    )
+  }
+}
 
-  const todoItems = todosData.map(item => <TodoItem key={item.id} item={item}/>)
-  return (
-    <div className="todo-list">
-      {todoItems}
-    </div>
-)
+// #2
+class Header extends React.Component {
+  render(){
+    return (
+      <header>
+          <p>Welcome, {this.props.username}!</p>
+      </header>
+    )
+  }
+}
+
+// #3
+class Greeting extends Component {
+  render(){
+    const date = new Date()
+    const hours = date.getHours()
+    let timeOfDay
+    
+    if (hours < 12) {
+        timeOfDay = "morning"
+    } else if (hours >= 12 && hours < 17) {
+        timeOfDay = "afternoon"
+    } else {
+        timeOfDay = "night"
+    }
+    return (
+        <h1>Good {timeOfDay} to you, sir or madam!</h1>
+    )
+  }
 }
 
 export default App;
