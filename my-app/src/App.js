05afbf2284d4fd8d72aae2c78c96254010410b46
Varin Upadhyay <varin6033@gmail.com>
2022-03-24 20:33:51 -0400

    componentDidUpdate practice

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,56 +1,39 @@
-/**
- * Let's make it so our checkbox can actually mark our todo as complete or incomplete!
- * This challenge is a little more involved than some of the past ones. Check the comments 
- * in the code for some help on accomplishing this one
- * 
- * Challenge: 
- * 1. Create an event handler in the App component for when the checkbox is clicked (which is an `onChange` event)
- *    a. This method will be the trickest part. Check the comments in the stubbed-out method below for some pseudocode to help guide you through this part
- * 2. Pass the method down to the TodoItem component
- * 3. In the TodoItem component, make it so when the `onChange` event happens, it calls the `handleChange` method and passes the id of the todo into the function
- */
-
 import React, {Component} from "react"
-import TodoItem from "./components/TodoItem"
-import todosData from "./components/todosData"
+import randomColor from "randomcolor"
 
 class App extends Component{
-    constructor() {
+    constructor(){
         super()
         this.state = {
-            todos: todosData
+            count: 0,
+            color: ""
         }
-        this.handleChange = this.handleChange.bind(this)
+        this.increment = this.increment.bind(this)
     }
 
-    handleChange(id){
-        // Update state so that the item with the given id flips `completed` from false to true (or vise versa)
-        // Remember not to modify prevState directly, but instead to return a new version of state with the change you want included in that update. (Think how you might use the `.map` method to do this)
+    increment() {
         this.setState(prevState => {
-            const updatedTodos = prevState.todos.map(todo => {
-                if(todo.id === id){
-                    return {
-                        ...todo,
-                        completed: !todo.completed
-                    }
-                }
-                return todo
-            })
-            console.log(prevState.todos)
-            console.log(updatedTodos)
             return {
-                todos : updatedTodos
+                count: prevState.count + 1
             }
         })
     }
 
-    render() {
-        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} 
-            handleChange={this.handleChange}/>)
+    componentDidUpdate(prevProps, prevState) {
+        if(prevState.count !== this.state.count){
+            const newColor = randomColor()
+            this.setState({color: newColor})
+        }
+        
+    }
 
+    render() {
         return (
-            <div className="todo-list">
-                {todoItems}
+            <div>
+                <h1 style={{color: this.state.color}}>{this.state.count}</h1>
+                <button onClick={this.increment}>
+                    increment!
+                </button>
             </div>
         )
     }
